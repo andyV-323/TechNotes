38f42e91612ff7244c9ee6ee493cdfd9c5ebf505
andyV-323 <40248914+andyV-323@users.noreply.github.com>
2023-09-12 20:37:11 -0700

    react/frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,32 @@
+import { Routes, Route } from 'react-router-dom';
+import Layout from './component/Layout';
+import Public from './component/Public';
+import Login from './features/auth/Login';
+import DashLayout from './component/DashLayout';
+import Welcome from './features/auth/Welcome';
+import NotesList from './features/notes/NotesList';
+import UsersList from './features/users/UsersList';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Public />} />
+        <Route path="login" element={<Login />} />
+
+        <Route path="dash" element={<DashLayout />}>
+          <Route index element={<Welcome />} />
+          <Route path="notes">
+            <Route index element={<NotesList />} />
+          </Route>
+          <Route path="users">
+            <Route index element={<UsersList />} />
+          </Route>
+        </Route>
+        {/*End Dash*/}
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
